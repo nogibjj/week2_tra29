0d5c04f9aa79419a69dd241fcec8999f23c9f669
titusrobin <143838819+titusrobin@users.noreply.github.com>
2023-09-12 21:18:25 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import polars as pl
 import matplotlib.pyplot as plt
 
-def polars_descriptive_stat_mean(df: pl.DataFrame, col: str) -> float:
+def descriptive_stat_mean(df: pl.DataFrame, col: str) -> float:
     return df[col].mean()
 
 def visualize_data(df):
@@ -15,5 +15,5 @@ def visualize_data(df):
 if __name__ == '__main__':
     cars = pl.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
     print(cars.head())
-    print(polars_descriptive_stat_mean(cars, 'mpg'))
+    print(descriptive_stat_mean(cars, 'mpg'))
     visualize_data(cars)
