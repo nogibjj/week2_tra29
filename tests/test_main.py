8dfdd15378f6edda936c14094151b2c6aac06759
titusrobin <143838819+titusrobin@users.noreply.github.com>
2023-09-12 23:00:10 -0400

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 import polars as pl
-from src.main import descriptive_stat
+from main import descriptive_stat, visualize_data
 
-def test_descriptive_stat():
+def test_descriptive_stat_mean():
     # Import the DataFrame from the CSV file
     cars = pl.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
 
@@ -18,9 +18,17 @@ def test_descriptive_stat():
     # Use assert to compare the calculated stats with the expected stats
     for col in expected_stats["summary"]:
         for column in ["mpg", "hp"]:
-            calculated_value = calculated_stats.filter(pl.col("summary") == col)[column][0]
+            calculated_value = calculated_stats[column].to_pandas().iloc[0]
             expected_value = expected_stats[column][expected_stats["summary"].index(col)]
             assert calculated_value == expected_value
 
+def test_visualize_data():
+    # Import the DataFrame from the CSV file
+    cars = pl.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
+
+    # Ensure the visualize_data function runs without errors
+    visualize_data(cars)
+
 if __name__ == "__main__":
-    test_descriptive_stat()
+    test_descriptive_stat_mean()
+    test_visualize_data()
