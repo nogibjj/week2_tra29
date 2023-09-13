869fc87b8b65d8e25ef82ba23e112ff99f413a94
titusrobin <your.email@example.com>
2023-09-12 20:35:09 -0400

    pandas descriptive stats

--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
-"""
-Some code for sanity checking template
-"""
+import polars as pl
+import matplotlib.pyplot as plt
 
-def sanity_add(x, y):
-    return x + y
+def polars_descriptive_stat_min(df: pl.DataFrame, col: str) -> float:
+    return df[col].min()  # Calculate the minimum value
+
+def polars_descriptive_stat_mean(df: pl.DataFrame, col: str) -> float:
+    return df[col].mean()  # Calculate the mean value
+
+def polars_descriptive_stat_max(df: pl.DataFrame, col: str) -> float:
+    return df[col].max()  # Calculate the maximum value
+
+def visualize_data(df):
+    plt.scatter(df["mpg"], df["hp"])
+    plt.xlabel("Miles Per Gallon")
+    plt.ylabel("Horse Power")
+    plt.title("Miles per gallon changes with automobile weight")
+    plt.show()
 
 if __name__ == '__main__':
-    a = 1
-    b = 2
-
-    print(sanity_add(a, b))
-  
+    cars = pl.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
+    print(cars.head())
+    
+    # Calculate and print the minimum, mean, and maximum values
+    print("Minimum:", polars_descriptive_stat_min(cars, 'mpg'))
+    print("Mean:", polars)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,49 @@
-from src.main import sanity_add
+import polars as pl
+from main import polars_descriptive_stat_min, polars_descriptive_stat_mean, polars_descriptive_stat_max
 
-def test_sanity_add():
-  assert sanity_add(2, 3) == 5
+def test_descriptive_statgit_min():
+    # Create a Polars DataFrame
+    cars = pl.DataFrame({
+        "mpg": [21.0, 21.0, 22.8, 21.4, 18.7],
+        "hp": [110, 110, 93, 110, 175]
+    })
+
+    target_column = "mpg"
+    min_mpg = polars_descriptive_stat_min(cars, target_column)
+
+    # Calculate the minimum manually
+    calculated_min = cars[target_column].min()
+    assert min_mpg == calculated_min
+
+def test_descriptive_stat_mean():
+    # Create a Polars DataFrame
+    cars = pl.DataFrame({
+        "mpg": [21.0, 21.0, 22.8, 21.4, 18.7],
+        "hp": [110, 110, 93, 110, 175]
+    })
+
+    target_column = "mpg"
+    mean_mpg = polars_descriptive_stat_mean(cars, target_column)
+
+    # Calculate the mean manually
+    calculated_mean = cars[target_column].mean()
+    assert mean_mpg == calculated_mean
+
+def test_descriptive_stat_max():
+    # Create a Polars DataFrame
+    cars = pl.DataFrame({
+        "mpg": [21.0, 21.0, 22.8, 21.4, 18.7],
+        "hp": [110, 110, 93, 110, 175]
+    })
+
+    target_column = "mpg"
+    max_mpg = polars_descriptive_stat_max(cars, target_column)
+
+    # Calculate the maximum manually
+    calculated_max = cars[target_column].max()
+    assert max_mpg == calculated_max
+
+if __name__ == "__main__":
+    test_descriptive_stat_min()
+    test_descriptive_stat_mean()
+    test_descriptive_stat_max()
