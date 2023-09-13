36054e580ddc579169ed4c8153bd00940b477299
titusrobin <143838819+titusrobin@users.noreply.github.com>
2023-09-12 21:28:57 -0400

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 from src.main import descriptive_stat_mean
+import polars as pl
 
 def test_descriptive_stat_mean():
     cars = pl.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
