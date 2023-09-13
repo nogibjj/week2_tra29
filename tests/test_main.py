8ca9cb1a87dc76af2354fa690a83658ce2cc0112
titusrobin <143838819+titusrobin@users.noreply.github.com>
2023-09-12 22:13:30 -0400

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,21 @@
-from src.main import descriptive_stat_mean
 import polars as pl
+import matplotlib.pyplot as plt
 
-def test_descriptive_stat_mean():
+def descriptive_stat_mean(df: pl.DataFrame, col: str) -> float:
+    #return df[col].mean()
+    summary_stats = df.describe()
+    return summary_stats
+
+def visualize_data(df):
+    plt.scatter(df["mpg"], df["hp"])
+    plt.xlabel("Miles Per Gallon")
+    plt.ylabel("Horse Power")
+    plt.title("Miles per gallon changes with automible weight")
+    plt.show()
+
+
+if __name__ == '__main__':
     cars = pl.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
-    target_column = "mpg"
-    mean_mpg = descriptive_stat_mean(cars, target_column)
-
-    calculated_mean = cars[target_column].sum()/len(cars[target_column])
-    assert mean_mpg == calculated_mean
-
-if __name__ == "__main__":
-    test_descriptive_stat_mean()
+    print(cars.head())
+    print(descriptive_stat_mean(cars, 'mpg'))
+    visualize_data(cars)
