a066523f88b97816bf68e3a6e6f7f8be0372e649
titusrobin <143838819+titusrobin@users.noreply.github.com>
2023-09-12 22:12:25 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,20 @@
 import polars as pl
-import matplotlib.pyplot as plt
 
-def descriptive_stat_mean(df: pl.DataFrame, col: str) -> float:
-    #return df[col].mean()
-    summary_stats = df.describe()
-    return summary_stats
-
-def visualize_data(df):
-    plt.scatter(df["mpg"], df["hp"])
-    plt.xlabel("Miles Per Gallon")
-    plt.ylabel("Horse Power")
-    plt.title("Miles per gallon changes with automible weight")
-    plt.show()
-
-
-if __name__ == '__main__':
+def test_descriptive_stat_mean():
+    # Import the DataFrame from the CSV file
     cars = pl.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
-    print(cars.head())
-    print(descriptive_stat_mean(cars, 'mpg'))
-    visualize_data(cars)
+
+    target_column = "mpg"
+    expected_mean = cars[target_column].mean()
+
+    # Calculate the mean using the function
+    calculated_stats = descriptive_stat_mean(cars, target_column)
+
+    # Extract the mean from the summary statistics
+    calculated_mean = calculated_stats[target_column].filter(pl.col("summary") == "mean").select(target_column).to_pandas().iloc[0, 0]
+
+    # Use assert to compare the calculated mean with the expected mean
+    assert calculated_mean == expected_mean
+
+if __name__ == "__main__":
+    test_descriptive_stat_mean()
