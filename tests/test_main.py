faff46a674e52f3fe951c2327dafa1bb04deaf9a
titusrobin <143838819+titusrobin@users.noreply.github.com>
2023-09-12 23:05:05 -0400

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 import polars as pl
 from src.main import descriptive_stat, visualize_data
 
-def test_descriptive_stat_mean():
+def test_descriptive_stat():
     # Import the DataFrame from the CSV file
     cars = pl.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
 
